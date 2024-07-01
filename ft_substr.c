c5ff4e1a4b618b8fd61dc543cafe808c383f7e53
aal-hawa <aal-hawa@student.42abudhabi.ae>
2024-07-01 18:07:08 +0400

    first commit

---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Anas Al Hawamda <aal-hawa@student.42abu    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 17:08:41 by Anas Al Haw       #+#    #+#             */
+/*   Updated: 2024/07/01 17:08:41 by Anas Al Haw      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char    *dst;
+    size_t     i;
+    size_t  sz;
+
+    dst = mallo(sizeof(char) * (len + 1));
+    if (!dst)
+        return (0);
+    i = 0;
+    sz = 0;
+    while (*s && i < start)
+       s++;
+    if (!*s)
+        return (0);
+    while (*s && sz < len)
+    {
+      *dst = *s;
+      sz++;
+      dst++;
+      s++;
+    }
+    *dst = 0;
+    return dst;
+}
