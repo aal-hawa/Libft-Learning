dd1f0d846d87418dbac66c61d436a740c486bc35
aal-hawa <aal-hawa@student.42abudhabi.ae>
2024-07-06 16:08:47 +0400

    add

---
+++ ft_lstadd_back.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Anas Al Hawamda <aal-hawa@student.42abu    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/06 15:46:52 by Anas Al Haw       #+#    #+#             */
+/*   Updated: 2024/07/06 15:46:52 by Anas Al Haw      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	lastl;
+
+	if (!lst)
+	{
+		*lst = new;
+		new->next = NULL;
+		return;
+	}
+	lastl = *lst;
+	while (lastl->next)
+			lastl = lastl->next;
+	lastl->next = new;
+	new->next = NULL;
+}
---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Anas Al Hawamda <aal-hawa@student.42abu    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/06 14:23:13 by Anas Al Haw       #+#    #+#             */
+/*   Updated: 2024/07/06 14:23:13 by Anas Al Haw      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	new_list;
+
+	new_list = (t_list *)malloc(sizeof(t_list));
+	if(!new_list)
+		return (NULL);
+	new_list->content = content;
+	new_list->next = NULL;
+	return (new_list);
+}
